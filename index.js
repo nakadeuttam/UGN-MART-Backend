ad237fb3d0ee1d7d07e0ad74b64cae6e8ef5b735
nakadeuttam <nakadeuttam66@gmail.com>
2024-10-20 02:17:41 +0530

    Initializing the Project Backend part, Added Signup And Signin Functionalities

---
+++ index.js
@@ -0,0 +1,153 @@
+require('dotenv').config();
+const port = process.env.PORT || 4000; 
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+app.use(express.json());    // node .\index.js
+app.use(cors());
+
+// Database Connection With MongoDB
+console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL); 
+
+
+// MiddleWare to fetch user from database
+const fetchuser = async (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ errors: "Please authenticate using a valid token" });
+  }
+  try {
+    const data = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = data.user;
+    next();
+  } catch (error) {
+    res.status(401).send({ errors: "Please authenticate using a valid token" });
+  }
+};
+
+
+// Schema for creating user model
+const Users = mongoose.model("Users", {
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  cartData: {
+    type: Object,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Schema for creating Product
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number
+  },
+  old_price: {
+    type: Number
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  avilable: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Root");
+});
+
+//Create an endpoint at ip/login for login the user and giving auth-token
+app.post('/login', async (req, res) => {
+  console.log("Login");
+    let success = false;
+    let user = await Users.findOne({ email: req.body.email });
+    if (user) {
+        const passCompare = req.body.password === user.password;
+        if (passCompare) {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+			success = true;
+      console.log(user.id);
+			const token = jwt.sign(data,process.env.SECRET_KEY);
+			res.json({ success, token });
+        }
+        else {
+            return res.status(400).json({success: success, errors: "please try with correct email/password"})
+        }
+    }
+    else {
+        return res.status(400).json({success: success, errors: "please try with correct email/password"})
+    }
+})
+
+//Create an endpoint at ip/auth for regestring the user in data base & sending token
+app.post('/signup', async (req, res) => {
+  console.log("Sign Up");
+        let success = false;
+        let check = await Users.findOne({ email: req.body.email });
+        if (check) {
+            return res.status(400).json({ success: success, errors: "existing user found with this email" });
+        }
+        let cart = {};
+          for (let i = 0; i < 300; i++) {
+          cart[i] = 0;
+        }
+        const user = new Users({
+            name: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            cartData: cart,
+        });
+        await user.save();
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        
+        const token = jwt.sign(data, process.env.SECRET_KEY);
+        success = true; 
+        res.json({ success, token })
+    })
+
+
+app.listen(port, (error) => {
+  if (!error) console.log("Server Running on port " + port);
+  else console.log("Error : ", error);
+});
