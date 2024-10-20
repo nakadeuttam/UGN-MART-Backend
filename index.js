c91b05d2ba6281d01b85ba5a91f76f2dea3c280f
nakadeuttam <nakadeuttam66@gmail.com>
2024-10-21 00:00:54 +0530

    Created Endpoint for uploading and accessing the product images (used multer npm)

--- index.js
+++ index.js
@@ -4,15 +4,32 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
 const cors = require("cors");
 
 app.use(express.json());    // node .\index.js
 app.use(cors());
 
 // Database Connection With MongoDB
-console.log(process.env.MONGO_URL)
 mongoose.connect(process.env.MONGO_URL); 
 
+//Image Storage Engine 
+const storage = multer.diskStorage({
+  destination: './upload/images',
+  filename: (req, file, cb) => {
+    console.log(file);
+      return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+  }
+})  
+const upload = multer({storage: storage})
+app.post("/upload", upload.single('product'), (req, res) => {
+  res.json({
+      success: 1,
+      image_url: `http://localhost:4000/images/${req.file.filename}`
+  })
+})
+app.use('/images', express.static('upload/images'));
 
 // MiddleWare to fetch user from database
 const fetchuser = async (req, res, next) => {
@@ -145,6 +162,13 @@ app.post('/signup', async (req, res) => {
         success = true; 
         res.json({ success, token })
     })
+
+    //endpoint to access all the products
+    app.get("/allproducts", async (req, res) => {
+      let products = await Product.find({});
+      console.log("All Products");
+        res.send(products);
+    });
 
 
 app.listen(port, (error) => {
